dc97f8b2d8074c0e84204ccb051a7c3300ab962d
Derek Christenson <derek.a.christenson@gmail.com>
2023-08-22 17:46:44 -0700

    To bottom p65; works.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-val kotlin_version = "1.4.30"
+val kotlin_version = "1.8.10"
 val nav_version = "2.7.0"
 val lc_version = "2.6.1"
 
@@ -55,13 +55,15 @@ android {
 
 dependencies {
 
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
-    implementation("androidx.lifecycle:lifecycle-livedata-core-ktx:$lc_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lc_version")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lc_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/pennydrop4/fragments/PickPlayersFragment.kt
+++ app/src/main/java/com/example/pennydrop4/fragments/PickPlayersFragment.kt
@@ -7,11 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pennydrop4.R
 import com.example.pennydrop4.databinding.FragmentPickPlayersBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.pennydrop4.viewmodels.PickPlayersViewModel
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass.
@@ -19,17 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class PickPlayersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val pickPlayersViewModel by activityViewModels<PickPlayersViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,27 +25,10 @@ class PickPlayersFragment : Fragment() {
     ): View? {
         val binding = FragmentPickPlayersBinding
             .inflate(inflater, container, false)
+            .apply {
+                this.vm = pickPlayersViewModel
+            }
 
         return binding.root
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PickPlayersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PickPlayersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
