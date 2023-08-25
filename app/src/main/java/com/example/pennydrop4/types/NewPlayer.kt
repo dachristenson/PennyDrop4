a1735cde30e13666db452c3b24458682d76a0d3a
Derek Christenson <dachris@asu.edu>
2023-08-24 17:00:41 -0700

    To bottom p77.

--- app/src/main/java/com/example/pennydrop4/types/NewPlayer.kt
+++ app/src/main/java/com/example/pennydrop4/types/NewPlayer.kt
@@ -1,6 +1,7 @@
 package com.example.pennydrop4.types
 
 import androidx.databinding.ObservableBoolean
+import com.example.pennydrop4.game.AI
 
 data class NewPlayer(
     var playerName: String = "",
@@ -8,4 +9,11 @@ data class NewPlayer(
     val canBeRemoved: Boolean = true,
     val canBeToggled: Boolean = true,
     var isIncluded: ObservableBoolean = ObservableBoolean(!canBeRemoved)
-)
+    var selectedAIPosition: Int = -1
+) {
+    fun selectedAI() = if (!isHuman.get()) {
+        AI.basicAI.getOrNull(selectedAIPosition)
+    } else {
+        null
+    }
+}
---
+++ app/src/main/java/com/example/pennydrop4/viewmodels/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.pennydrop4.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+    // Logic will go in here
+}
