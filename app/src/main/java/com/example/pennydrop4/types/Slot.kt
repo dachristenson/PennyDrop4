42821d8881ec31bdeafd787cdb75417d9b159749
Derek Christenson <dachris@asu.edu>
2023-08-25 18:55:39 -0700

    To end Ch 3.

--- app/src/main/java/com/example/pennydrop4/types/Slot.kt
+++ app/src/main/java/com/example/pennydrop4/types/Slot.kt
@@ -3,6 +3,6 @@ package com.example.pennydrop4.types
 data class Slot (
     val number: Int,
     val canBeFilled: Boolean = true,
-    var isFilled: Boolean = number % 2 == 0,
-    var lastRolled: Boolean = number % 3 == 2
+    var isFilled: Boolean = false,
+    var lastRolled: Boolean = false
 )
