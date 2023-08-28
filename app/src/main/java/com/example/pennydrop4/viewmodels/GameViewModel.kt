e30050d7ce2f1850acf9a1ef2a550df3760c7ea5
Derek Christenson <derek.a.christenson@gmail.com>
2023-08-28 03:50:37 -0700

    To top p106; untested.

--- app/src/main/java/com/example/pennydrop4/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/example/pennydrop4/viewmodels/GameViewModel.kt
@@ -2,8 +2,12 @@ package com.example.pennydrop4.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.MutableLiveData
+import com.example.pennydrop4.game.GameHandler
+import com.example.pennydrop4.game.TurnEnd
+import com.example.pennydrop4.game.TurnResult
 import com.example.pennydrop4.types.Player
 import com.example.pennydrop4.types.Slot
+import com.example.pennydrop4.types.clear
 
 class GameViewModel: ViewModel() {
     private var players: List<Player> = emptyList()
@@ -30,14 +34,102 @@ class GameViewModel: ViewModel() {
                 this?.isRolling = true
             }
 
-        this.canRoll.value = true
+        canRoll.value = true
+        canPass.value = false
+
+        slots.value?.clear()
+        slots.notifyChange()
+
+        currentTurnText.value = "The game has begun!\n"
+        currentStandingsText.value = generateCurrentStandings(this.players)
     }
 
     fun roll() {
-        // Implementing later
+        slots.value?.let { currentSlots ->
+            // Comparing against true saves us a null check
+            val currentPlayer = players.firstOrNull { it.isRolling }
+
+            if (currentPlayer != null && canRoll.value == true) {
+                updateFromGameHandler(
+                    GameHandler.roll(players, currentPlayer, currentSlots)
+                )
+            }
+        }
     }
 
     fun pass() {
-        // Implementing later
+        val currentPlayer = players.firstOrNull { it.isRolling }
+
+        if (currentPlayer != null && canPass.value == true) {
+            updateFromGameHandler(GameHandler.pass(players, currentPlayer))
+        }
     }
+
+    private fun updateFromGameHandler(result: TurnResult) {
+        if (result.currentPlayer != null) {
+            currentPlayer.value?.addPennies(result.coinChangeCount ?: 0)
+            currentPlayer.value = result.currentPlayer
+
+            this.players.forEach { player ->
+                player.isRolling = result.currentPlayer == player
+            }
+        }
+
+        if (result.lastRoll != null) {
+            slots.value?.let { currentSlots ->
+                updateSlots(result, currentSlots, result.lastRoll)
+            }
+        }
+
+        currentTurnText.value = generateTurnText(result)
+        currentStandingsText.value = generateCurrentStandings(this.players)
+
+        canRoll.value = result.canRoll
+        canPass.value = result.canPass
+
+        if (!result.isGameOver && result.currentPlayer?.isHuman == false) {
+            canRoll.value = false
+            canPass.value = false
+        }
+    }
+
+    private fun updateSlots(
+        result: TurnResult,
+        currentSlots: List<Slot>,
+        lastRoll: Int
+    ) {
+        if (result.clearSlots) {
+            currentSlots.clear()
+        }
+
+        currentSlots.firstOrNull { it.lastRolled }?.apply { lastRolled = false }
+
+        currentSlots.getOrNull(lastRoll - 1)?.also { slot ->
+            if (!result.clearSlots && slot.canBeFilled) slot.isFilled = true
+
+            slot.lastRolled = true
+        }
+
+        slots.notifyChange()
+    }
+
+    private fun generateTurnText(result: TurnResult): String {
+        // INSERT REAL FUNCTION DEFINITION BELOW
+        return "PLACEHOLDER"
+    }
+
+    private fun<T> MutableLiveData<List<T>>.notifyChange() {
+        this.value = this.value
+    }
+
+    private fun generateCurrentStandings(
+        players: List<Player>,
+        headerText: String = "Current Standings:"
+    ) =
+        players.sortedBy { it.pennies }.joinToString(
+            separator = "\n",
+            prefix = "$headerText\n"
+        ) {
+            "\t${it.playerName} - ${it.pennies} pennies"
+        }
 }
